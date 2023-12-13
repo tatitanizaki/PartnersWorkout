be5dc4331d57eb675ad8e9b827fd92a60d9b0d2b
tatitanizaki <tatiana.tanizaki@gmail.com>
2023-12-13 13:17:44 -0500

    Fixing Nested Navigator on StackNavigator.js

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,16 +1,18 @@
 // StackNavigator.js
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import App from './App';
+import WorkoutScreen from './WorkoutScreen';
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={App} />
+      <Stack.Screen name="Home" component={WorkoutScreen} />
+      {/* Add more screens as needed */}
     </Stack.Navigator>
   );
 };
 
 export default StackNavigator;
+
