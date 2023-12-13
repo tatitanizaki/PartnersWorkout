e7e6b8dfbed10d9f7beb8f72239a77dda266e855
tatitanizaki <tatiana.tanizaki@gmail.com>
2023-12-13 13:31:37 -0500

    workout data is correctly passed from StackNavigat

--- StackNavigator.js
+++ StackNavigator.js
@@ -5,14 +5,17 @@ import WorkoutScreen from './WorkoutScreen';
 
 const Stack = createStackNavigator();
 
-const StackNavigator = () => {
+const StackNavigator = ({ workout }) => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={WorkoutScreen} />
+      <Stack.Screen name="Home" options={{ title: 'Today\'s Workout' }}>
+        {(props) => <WorkoutScreen {...props} workout={workout} />}
+      </Stack.Screen>
       {/* Add more screens as needed */}
     </Stack.Navigator>
   );
 };
 
 export default StackNavigator;
+
 
